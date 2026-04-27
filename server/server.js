862d7e86374d301659e6f063b70667e4f5c47d0c
Akilo Akolu <a_akolu@fansahweonline.ca>
2026-04-26 21:46:13 -0400

    Update server port for Render

--- server/server.js
+++ server/server.js
@@ -55,6 +55,8 @@ app.post("/analyze", (req, res) => {
   res.json(result);
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
